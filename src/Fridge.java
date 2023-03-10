ceba18b9e3f625c33d5cae8c77ea311e8e483a42
Melanie Voloshynska <m.voloshynska@gmail.com>
2023-03-10 21:19:53 +0200

    Updating code

--- src/Fridge.java
+++ src/Fridge.java
@@ -1,11 +1,36 @@
 import lombok.*;
+@ToString
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class Fridge {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private String brand;
+    private String model;
+    private int capacityInLitres;
+    private boolean isDefrosting;
+    private String energyEfficiencyClass;
+
+
+    public void turnOnDefrosting()
+    {
+        isDefrosting = true;
     }
-}
+    public void turnOffDefrosting()
+    {
+        isDefrosting = false;
+    }
+    public void defrostStatus()
+    {
+        if(isDefrosting == true)
+        {
+            System. out. println("defrost is on");
+        }
+        else
+        {
+            System. out. println("defrost is off");
+        }
+    }
+
+    }
