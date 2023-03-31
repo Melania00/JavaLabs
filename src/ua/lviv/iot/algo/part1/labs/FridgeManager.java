70399aae596601bd37405109ed961e3fd4f49c8e
Melanie Voloshynska <m.voloshynska@gmail.com>
2023-04-01 02:40:05 +0300

    Pushing 2 lab code

---
+++ src/ua/lviv/iot/algo/part1/labs/FridgeManager.java
@@ -0,0 +1,37 @@
+package ua.lviv.iot.algo.part1.labs;
+import lombok.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@ToString
+@Getter
+
+public class FridgeManager {
+    private static LinkedList<FridgeCamera> fridgesCameraList;
+    private static int[] fridgeCameraCapacity = {3, 5, 1};
+    private static String[] brand = {"Nexus","Samsung","Apple"};
+    private static int[] volumePerKilogram= {2,4,5};
+
+    private static LinkedList<WineFridge> wineFridges;
+    private static int[] fridgeWineCapacity = {1,2,3};
+
+    public static void main(String[] args) {
+        fridgesCameraList = new LinkedList<>();
+        for(int i = 0; i < fridgeCameraCapacity.length; i++){
+            fridgesCameraList.add(new FridgeCamera(brand[i],"daabifb134ascfq",fridgeCameraCapacity[i],true,"A",1,"mechanic",20,1,15));
+        }
+
+        wineFridges = new LinkedList<>();
+        for(int i = 0; i < fridgeWineCapacity.length; i++){
+            wineFridges.add(new WineFridge("Nexus","ff4hh8",fridgeWineCapacity[i],false, "B", 40, 2));
+        }
+
+        for (FridgeCamera item : fridgesCameraList) {
+            item.toString(item);
+        }
+        for (WineFridge item : wineFridges) {
+            item.toString(item);
+        }
+    }
+}
