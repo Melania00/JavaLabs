1db4088d350fbb587e163dece9331e0f3d47cfe6
Melanie Voloshynska <m.voloshynska@gmail.com>
2023-04-06 22:35:50 +0300

    Installing maven

--- src/ua/lviv/iot/algo/part1/labs/FridgeManager.java
+++ src/ua/lviv/iot/algo/part1/labs/FridgeManager.java
@@ -1,7 +1,7 @@
 package ua.lviv.iot.algo.part1.labs;
 import lombok.*;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 @ToString
@@ -13,16 +13,16 @@ public class FridgeManager {
     private static String[] brand = {"Nexus","Samsung","Apple"};
     private static int[] volumePerKilogram= {2,4,5};
 
-    private static LinkedList<WineFridge> wineFridges;
+    private static ArrayList<WineFridge> wineFridges;
     private static final int[] fridgeWineCapacity = {1,2,3};
 
     public static void main(String[] args) {
-        fridgesCameraList = new LinkedList<>();
+        fridgesCameraList = new ArrayList<>();
         for(int i = 0; i < fridgeCameraCapacity.length; i++){
             fridgesCameraList.add(new FridgeCamera(brand[i],"dfy677y",fridgeCameraCapacity[i],true,"A",1,"mechanic",20,1,15));
         }
 
-        wineFridges = new LinkedList<>();
+        wineFridges = new ArrayList<>();
         for(int i = 0; i < fridgeWineCapacity.length; i++){
             wineFridges.add(new WineFridge(brand[i],"ff4hh8",fridgeWineCapacity[i],false, "B", 40, 2));
         }
