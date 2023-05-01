4435c05dcd9c6acc409cc24fce01a1d535ba2932
Melanie Voloshynska <m.voloshynska@gmail.com>
2023-05-01 14:09:44 +0300

    Changing ArrayList to LinkedList

--- src/main/java/ua/lviv/iot/algo/part1/labs/writers/HyphenatedWords.java
+++ src/main/java/ua/lviv/iot/algo/part1/labs/writers/HyphenatedWords.java
@@ -1,9 +1,11 @@
 package ua.lviv.iot.algo.part1.labs.writers;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
 import java.util.Collections;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 public class HyphenatedWords{
     private final String text;
@@ -12,8 +14,8 @@ public class HyphenatedWords{
         this.text = text;
     }
 
-    public ArrayList<String> findHyphenatedWords() {
-        ArrayList<String> hyphenWords = new ArrayList<String>();
+    public List<String> findHyphenatedWords() {
+        List<String> hyphenWords = new LinkedList<String>();
         Pattern pattern = Pattern.compile("\\b\\w+-\\w+\\b");
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
@@ -22,20 +24,20 @@ public class HyphenatedWords{
         Collections.sort(hyphenWords);
         return hyphenWords;
     }
+
     public static void main(String[] args) {
-        String text = "In the world of business, it's important to stay up-to-date with the latest technologies and strategies.\n" +
-                " One way to do this is by attending industry-leading conferences and events.\n" +
+        String text = "In the world of business, it's important to stay up-to-date with the latest technologies and strategies.%n" +
+                " One way to do this is by attending industry-leading conferences and events.%n" +
                 " These events offer a unique opportunity to network with like-minded professionals and gain valuable " +
-                "insights into the latest trends.\n Another key aspect of successful business is effective communication.\n" +
+                "insights into the latest trends.%n Another key aspect of successful business is effective communication.%n" +
                 " This includes not only clear and concise verbal communication, but also well-written documents such" +
-                " as reports, proposals, and presentations.\n By using appropriate hyphenated words, you can ensure that" +
-                " your audience fully understands your message and is more likely to take action.\n Remember to stay" +
+                " as reports, proposals, and presentations.%n By using appropriate hyphenated words, you can ensure that" +
+                " your audience fully understands your message and is more likely to take action.%n Remember to stay" +
                 " hyphenated and stay ahead of the game.";
         HyphenatedWords hyphenWordsFinder = new HyphenatedWords(text);
-        ArrayList<String> hyphenWords = hyphenWordsFinder.findHyphenatedWords();
+        List<String> hyphenWords = hyphenWordsFinder.findHyphenatedWords();
         for (String word : hyphenWords) {
             System.out.println(word);
         }
-    }}
-
-
+    }
+}
--- src/test/java/ua/lviv/iot/algo/part1/labs/writers/HyphenatedWordsTest.java
+++ src/test/java/ua/lviv/iot/algo/part1/labs/writers/HyphenatedWordsTest.java
@@ -3,14 +3,14 @@ package ua.lviv.iot.algo.part1.labs.writers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class HyphenatedWordsTest {
 
     @Test
     public void findHyphenatedWords_emptyText_returnsEmptyList() {
         HyphenatedWords hyphenatedWords = new HyphenatedWords("");
-        ArrayList<String> hyphenated = hyphenatedWords.findHyphenatedWords();
+        List<String> hyphenated = hyphenatedWords.findHyphenatedWords();
         Assertions.assertEquals(0, hyphenated.size());
     }
 
@@ -18,7 +18,7 @@ public class HyphenatedWordsTest {
     public void findHyphenatedWords_textWithNoHyphens_returnsEmptyList() {
         String text = "This text has no hyphens.";
         HyphenatedWords hyphenatedWords = new HyphenatedWords(text);
-        ArrayList<String> hyphenated = hyphenatedWords.findHyphenatedWords();
+        List<String> hyphenated = hyphenatedWords.findHyphenatedWords();
         Assertions.assertEquals(0, hyphenated.size());
     }
 
@@ -26,16 +26,16 @@ public class HyphenatedWordsTest {
     public void findHyphenatedWords_textWithHyphens_returnsHyphenatedWords() {
         String text = "This is a text with some hyphenated-words.";
         HyphenatedWords hyphenatedWords = new HyphenatedWords(text);
-        ArrayList<String> hyphenated = hyphenatedWords.findHyphenatedWords();
+        List<String> hyphenated = hyphenatedWords.findHyphenatedWords();
         Assertions.assertEquals(1, hyphenated.size());
         Assertions.assertEquals("hyphenated-words", hyphenated.get(0));
     }
 
     @Test
     public void findHyphenatedWords_textWithMultipleHyphens_returnsSortedHyphenatedWords() {
-        String text = "This is a text with some hyphenated-words and some other re-used-words.";
+        String text = "This is a text with some hyphenated-words and some other re-used words.";
         HyphenatedWords hyphenatedWords = new HyphenatedWords(text);
-        ArrayList<String> hyphenated = hyphenatedWords.findHyphenatedWords();
+        List<String> hyphenated = hyphenatedWords.findHyphenatedWords();
         Assertions.assertEquals(2, hyphenated.size());
         Assertions.assertEquals("hyphenated-words", hyphenated.get(0));
         Assertions.assertEquals("re-used", hyphenated.get(1));
